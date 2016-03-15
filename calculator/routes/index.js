f2dc4e82ba419d97fea0d6d1276c5b3e186791e3
shinesoleil <hao_pan@outlook.com>
2016-03-15 14:55:18 +0800

    add sum api

--- calculator/routes/index.js
+++ calculator/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/sum', function(req, res, next) {
+  var firstNum = req.body.firstNum;
+  var secondNum = req.body.secondNum;
+
+  var result = parseFloat(firstNum) + parseFloat(secondNum);
+
+  res.send(result.toString());
 });
 
 module.exports = router;
