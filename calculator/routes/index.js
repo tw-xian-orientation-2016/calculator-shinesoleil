7c30f68c9962a03464c184f31ae1d6a98057381d
shinesoleil <hao_pan@outlook.com>
2016-03-15 14:56:08 +0800

    add difference api

--- calculator/routes/index.js
+++ calculator/routes/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.post('/sum', function(req, res, next) {
   var firstNum = req.body.firstNum;
   var secondNum = req.body.secondNum;
 
   var result = parseFloat(firstNum) + parseFloat(secondNum);
+
+  res.send(result.toString());
+});
+
+router.post('/diff', function(req, res, next) {
+  var firstNum = req.body.firstNum;
+  var secondNum = req.body.secondNum;
+
+  var result = parseFloat(firstNum) - parseFloat(secondNum);
 
   res.send(result.toString());
 });
