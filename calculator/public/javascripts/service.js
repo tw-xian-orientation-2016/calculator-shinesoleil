e826a71736f832fe29a23ab3901a2aea24f30ffa
shinesoleil <hao_pan@outlook.com>
2016-03-16 08:01:06 +0800

    add service methods

--- calculator/public/javascripts/service.js
+++ calculator/public/javascripts/service.js
@@ -17,6 +17,42 @@ Service.prototype.getSum = function (service, callback) {
     });
 };
 
+Service.prototype.getDiff = function (service, callback) {
+  $.ajax({
+      type: 'POST',
+      url: '/diff',
+      data: {firstNum: this.firstNum, secondNum: this.secondNum}
+    })
+    .done(function (result) {
+      service.firstNum = result;
+      callback(result);
+    });
+};
+
+Service.prototype.getProduct = function (service, callback) {
+  $.ajax({
+      type: 'POST',
+      url: '/product',
+      data: {firstNum: this.firstNum, secondNum: this.secondNum}
+    })
+    .done(function (result) {
+      service.firstNum = result;
+      callback(result);
+    });
+};
+
+Service.prototype.getQuotient = function (service, callback) {
+  $.ajax({
+      type: 'POST',
+      url: '/quotient',
+      data: {firstNum: this.firstNum, secondNum: this.secondNum}
+    })
+    .done(function (result) {
+      service.firstNum = result;
+      callback(result);
+    });
+};
+
 Service.prototype.getMinus = function (callback) {
   $.ajax({
       type: 'POST',
